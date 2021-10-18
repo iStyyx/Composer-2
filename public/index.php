85c40101e7edea305bcb43519773e405e55fa897
Davy M <87863422+iStyyx@users.noreply.github.com>
2021-10-18 11:03:17 +0200

    addind all files for composer quest 2

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+    require_once '../src/wcs/Hello.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Composer - 2</title>
+</head>
+<body>
+    <?php
+
+        $hello = new Hello();
+        echo $hello->talk();
+
+    ?>
+</body>
+</html>
